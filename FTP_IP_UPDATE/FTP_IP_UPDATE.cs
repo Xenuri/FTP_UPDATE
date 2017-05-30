6ee7410159e361c42e9cb2b22510974d26a8b30f
Xenuri <dangibbs184@gmail.com>
2017-05-30 16:41:29 +0100

    Add files via upload
    Can't see any comments in code? It's not a bug, it's a feature! ;)

---
+++ FTP_IP_UPDATE/FTP_IP_UPDATE.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using Microsoft.Web.Administration;
+using System.IO;
+using System.Timers;
+
+
+namespace FTP_IP_UPDATE
+{
+    public partial class UpdateTool : ServiceBase
+    {
+        private Timer timer1 = null;
+
+        public UpdateTool()
+        {
+            InitializeComponent();
+
+            // Create the source, if it does not already exist.
+            if (!EventLog.SourceExists("FTP Update Tool"))
+            {
+                //An event log source should not be created and immediately used.
+                //There is a latency time to enable the source, it should be created
+                //prior to executing the application that uses the source.
+                //Execute this sample a second time to use the new source.
+                EventLog.CreateEventSource("FTP Update Tool", "XENET");
+            }
+
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            Library.WriteErrorLog("IP Update Service Started");
+            Library.WriteEventLog("IP Update Service Started", EventLogEntryType.Information);
+            timer1 = new Timer();
+
+            float ServiceUpdateRate = 0;
+            if (System.Configuration.ConfigurationManager.AppSettings["ServiceUpdateRate"] == "")
+            {
+                ServiceUpdateRate = 5;
+                Library.WriteErrorLog("ServiceUpdateRate setting is blank, Defaulting to 5.");
+                Library.WriteEventLog("ServiceUpdateRate setting is blank, Defaulting to 5.", EventLogEntryType.Warning);
+            }
+            else if (!float.TryParse(System.Configuration.ConfigurationManager.AppSettings["ServiceUpdateRate"], out ServiceUpdateRate))
+            {
+                ServiceUpdateRate =  5;
+                Library.WriteErrorLog("ServiceUpdateRate needs to be a number! Defaulting to 5.");
+                Library.WriteEventLog("ServiceUpdateRate needs to be a number! Defaulting to 5.", EventLogEntryType.Warning);
+            }
+            else
+            {
+                ServiceUpdateRate = float.Parse(System.Configuration.ConfigurationManager.AppSettings["ServiceUpdateRate"]);
+            }
+
+            this.timer1.Interval = ServiceUpdateRate * 60000;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_tick);
+            timer1.Enabled = true;
+        }
+
+        private void timer1_tick(object sender, ElapsedEventArgs e)
+        {
+            Library.UpdateIIS();
+        }
+
+
+        protected override void OnStop()
+        {
+            Library.WriteErrorLog("IP Update Service Stopped <--!");
+            Library.WriteEventLog("IP Update Service Stopped", EventLogEntryType.Warning);
+        }
+    }
+
+}
---
+++ FTP_IP_UPDATE/Library.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.IO;
+using Microsoft.Web.Administration;
+using System.Diagnostics;
+using System.Net;
+
+
+namespace FTP_IP_UPDATE
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(SystemException ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+
+            catch (UnauthorizedAccessException e)
+            {
+                WriteEventLog(e);
+            }
+            catch (SystemException e)
+            {
+                WriteEventLog(e);
+                WriteErrorLog(e);
+            }
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                WriteEventLog(e);
+            }
+            catch (SystemException e)
+            {
+                WriteEventLog(e);
+                WriteErrorLog(e);
+            }
+        }
+
+        public static void WriteEventLog(string Message, EventLogEntryType type)
+        {
+            EventLog myLog = new EventLog();
+            myLog.Source = "FTP Update Tool";
+            myLog.WriteEntry(Message, type);
+        }
+
+        public static void WriteEventLog(SystemException ex)
+        {
+            EventLog myLog = new EventLog();
+            myLog.Source = "FTP Update Tool";
+            myLog.WriteEntry(ex.Message, EventLogEntryType.Error);
+        }
+
+        public static void WriteEventLog(UnauthorizedAccessException ex)
+        {
+            EventLog myLog = new EventLog();
+            myLog.Source = "FTP Update Tool";
+            myLog.WriteEntry(ex.Message, EventLogEntryType.Error);
+        }
+        
+
+
+
+        public static void UpdateIIS()
+        {
+            if (!IsIPNew(GetPubIP(), GetIISIP()))
+            {
+                using (ServerManager serverManager = new ServerManager())
+                {
+                    Configuration config = serverManager.GetApplicationHostConfiguration();
+                    ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
+                    ConfigurationElementCollection sitesCollection = sitesSection.GetCollection();
+
+                    string SiteName = System.Configuration.ConfigurationManager.AppSettings["FTPSiteName"];
+                    ConfigurationElement siteElement = FindElement(sitesCollection, "site", "name", SiteName);
+                    try
+                    {
+                        if (siteElement == null) throw new InvalidOperationException("Site '" + SiteName + "' not found! Check Application config.");
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        WriteEventLog(e);
+                        WriteErrorLog(e);
+                    }
+                    ConfigurationElement ftpServerElement = siteElement.GetChildElement("ftpServer");
+                    ConfigurationElement firewallSupportElement = ftpServerElement.GetChildElement("firewallSupport");
+                    firewallSupportElement["externalIp4Address"] = GetPubIP();
+                    serverManager.CommitChanges();
+
+                    WriteErrorLog("Public IP change. Updating IIS with new IP: " + GetIISIP());
+                    WriteEventLog("Public IP change. Updating IIS with new IP: " + GetIISIP(), EventLogEntryType.Information);
+                }
+            }
+            else
+            {
+                WriteEventLog("No IP change", EventLogEntryType.Information);
+                WriteErrorLog("No IP change");
+            }
+        }
+
+
+        public static string GetPubIP()
+        {
+            try
+            {
+                string address = System.Configuration.ConfigurationManager.AppSettings["WebAddressEndpoint"];
+                WebRequest request = WebRequest.Create(address);
+                using (WebResponse response = request.GetResponse())
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    address = stream.ReadToEnd();
+                }
+
+                //Search for the ip in the html
+                int first = address.IndexOf("Address: ") + 9;
+                int last = address.LastIndexOf("");
+                address = address.Substring(first, last - first - 15);
+                return address;
+            }
+            catch (System.Configuration.ConfigurationErrorsException e)
+            {
+                WriteErrorLog(e);
+                WriteEventLog(e);
+                return null;
+            }
+            catch (System.SystemException e)
+            {
+                WriteErrorLog(e);
+                WriteEventLog(e);
+                return null;
+            }
+
+
+
+        }
+
+        public static string GetIISIP()
+        {
+
+            ServerManager serverManager = new ServerManager();
+            Configuration config = serverManager.GetApplicationHostConfiguration();
+            ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
+            ConfigurationElementCollection sitesCollection = sitesSection.GetCollection();
+
+            string FTPSiteName = System.Configuration.ConfigurationManager.AppSettings["FTPSiteName"];
+
+            ConfigurationElement siteElement = FindElement(sitesCollection, "site", "name", FTPSiteName);
+            try
+            {
+                if (siteElement == null) throw new InvalidOperationException("Site '" + FTPSiteName + "' not found! Check Application config.");
+            }
+            catch (InvalidOperationException e)
+            {
+                WriteEventLog(e);
+                WriteErrorLog(e);
+            }
+            ConfigurationElement ftpServerElement = siteElement.GetChildElement("ftpServer");
+
+            ConfigurationElement firewallSupportElement = ftpServerElement.GetChildElement("firewallSupport");
+            string iis_ip = (string)firewallSupportElement.Attributes["externalIp4Address"].Value;
+            return iis_ip;
+        }
+            
+
+
+
+        public static ConfigurationElement FindElement(ConfigurationElementCollection collection, string elementTagName, params string[] keyValues)
+        {
+            foreach (ConfigurationElement element in collection)
+            {
+                if (String.Equals(element.ElementTagName, elementTagName, StringComparison.OrdinalIgnoreCase))
+                {
+                    bool matches = true;
+                    for (int i = 0; i < keyValues.Length; i += 2)
+                    {
+                        object o = element.GetAttributeValue(keyValues[i]);
+                        string value = null;
+                        if (o != null)
+                        {
+                            value = o.ToString();
+                        }
+                        if (!String.Equals(value, keyValues[i + 1], StringComparison.OrdinalIgnoreCase))
+                        {
+                            matches = false;
+                            break;
+                        }
+                    }
+                    if (matches)
+                    {
+                        return element;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static bool IsIPNew(string GetPubIP, string GetIISIP)
+        {
+            bool result = GetPubIP.Equals(GetIISIP, StringComparison.Ordinal);
+            return result;
+        }
+
+
+
+    }
+}
